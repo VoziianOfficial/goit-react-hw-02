227fb23aedb383b27564d5375b5cd4eb6348d726
Yuliia Voziian <vozianulia@gmail.com>
2024-09-15 13:04:00 +0300

    update useEffect localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
-import { useEffect, useState } from 'react';
-import Description from './components/Description/Description';
-import Options from './components/Options/Options';
-import Feedback from './components/Feedback/Feedback';
-import Notification from './components/Notification/Notification';
-import './App.module.css/App.css';
+import { useEffect, useState } from "react";
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
+import "./App.module.css/App.css";
 
 function App() {
-  const [feedback, setFeedBack] = useState({
-    good: 0,
-	  neutral: 0,
-	  bad: 0,
-  });
-
-
-   // Завантаженя стану з localStorage при завантажувані сторінки
-  useEffect (() => {
-    const saveFeedback = localStorage.getItem('feedback');
-    if(saveFeedback) {
-      setFeedBack(JSON.parse(saveFeedback));
-    }
-  },[]);
+  // Завантаженя стану з localStorage при завантажувані сторінки
+  const [feedback, setFeedBack] = useState(
+    () =>
+      JSON.parse(localStorage.getItem("feedback")) ?? {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      }
+  );
 
   // Збереження в localStorage при кожній зміні
 
   useEffect(() => {
-    localStorage.setItem('feedback', JSON.stringify(feedback));
-  },[feedback]);
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
 
   // Функція для оновлення стану відгуків
   const updateFeedback = (feedbackTyp) => {
@@ -38,43 +33,42 @@ function App() {
   // Обчислення загальної кількості відгуків
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
-  // Процент позитивних відгуків 
+  // Процент позитивних відгуків
 
-  const positivePercentage = totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
+  const positivePercentage =
+    totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
 
-  // функція для скиданя відгуків 
-   const resetFeedback = () => {
+  // функція для скиданя відгуків
+  const resetFeedback = () => {
     setFeedBack({
       good: 0,
       neutral: 0,
       bad: 0,
-    })
-   }
+    });
+  };
 
   return (
     <>
-    <Description/>
-    <Options 
-    onLiveFeedback={updateFeedback}
-    onReset ={resetFeedback}
-    totalFeedback ={totalFeedback}
-     />
-
-    {totalFeedback > 0 ? (
-      <Feedback 
-      good={feedback.good} 
-      neutral={feedback.neutral} 
-      bad={feedback.bad}
-      total={totalFeedback}
-      positivePercentage={positivePercentage}
+      <Description />
+      <Options
+        onLiveFeedback={updateFeedback}
+        onReset={resetFeedback}
+        totalFeedback={totalFeedback}
       />
-    ) : (
-      <Notification message="No feedback yet"/>
-    )}
-  
 
+      {totalFeedback > 0 ? (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={totalFeedback}
+          positivePercentage={positivePercentage}
+        />
+      ) : (
+        <Notification message="No feedback yet" />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
